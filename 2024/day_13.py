93cc7ca2e6520b1f45a9432b27e731c4c3f96816
Viktor Nikolaus <Viktor.Nikolaus@gmx.net>
2024-12-13 17:50:42 +0100

    Add day 13 part 2

--- 2024/day_13.py
+++ 2024/day_13.py
@@ -1,24 +1,30 @@
 """Day 13"""
 
+from math import ceil, floor
+
 from common import get_puzzle_input
 
 PUZZLE_INPUT = get_puzzle_input()
+
+
+def parse_claw_machine(lines):
+    b_a = [l[len("Button A: "):] for l in lines if l.startswith("Button A: ")][0].split(", ")
+    b_b = [l[len("Button B: "):] for l in lines if l.startswith("Button B: ")][0].split(", ")
+    prize = [l[len("Prize: "):] for l in lines if l.startswith("Prize: ")][0].split(", ")
+    b_a = {p[0]: int(p[1:]) for p in b_a}
+    b_b = {p[0]: int(p[1:]) for p in b_b}
+    prize = {p[0]: int(p[2:]) for p in prize}
+    b_a = (b_a["X"], b_a["Y"])
+    b_b = (b_b["X"], b_b["Y"])
+    prize = (prize["X"], prize["Y"])
+    return (b_a, b_b, prize)
 
 
 def part_1():
     total_tokens = 0
     claw_machines = [cm.split("\n") for cm in PUZZLE_INPUT.split("\n\n")]
     for cm in claw_machines:
-        b_a = [l[len("Button A: "):] for l in cm if l.startswith("Button A: ")][0].split(", ")
-        b_b = [l[len("Button B: "):] for l in cm if l.startswith("Button B: ")][0].split(", ")
-        prize = [l[len("Prize: "):] for l in cm if l.startswith("Prize: ")][0].split(", ")
-        b_a = {p[0]: int(p[1:]) for p in b_a}
-        b_b = {p[0]: int(p[1:]) for p in b_b}
-        prize = {p[0]: int(p[2:]) for p in prize}
-        b_a = (b_a["X"], b_a["Y"])
-        b_b = (b_b["X"], b_b["Y"])
-        prize = (prize["X"], prize["Y"])
-
+        b_a, b_b, prize = parse_claw_machine(cm)
         combos = []
         for i in range(101):
             c_b = i
@@ -31,55 +37,43 @@ def part_1():
                 combos.append((int(c_a_x), c_b))
         if len(combos) == 0:
             continue
+        print(combos)
         min_tokens = min(c[0] * 3 + c[1] for c in combos)
         total_tokens += min_tokens
     print(total_tokens)
 
 
-# defect
+def get_vector_factors(target, vec_a, vec_b):
+    # t_x = vec_a_x * c_a + vec_b_x * c_b
+    # t_x - vec_b_x * c_b = vec_a_x * c_a
+    # (t_x - vec_b_x * c_b) / vec_a_x = c_a
+    # (t_y - vec_b_y * c_b) / vec_a_y = c_a
+
+    # (t_x - vec_b_x * c_b) / vec_a_x = (t_y - vec_b_y * c_b) / vec_a_y
+    # (t_x - vec_b_x * c_b) * vec_a_y = (t_y - vec_b_y * c_b) * vec_a_x
+    # t_x * vec_a_y - vec_b_x * c_b * vec_a_y = t_y * vec_a_x - vec_b_y * c_b * vec_a_x
+    # t_x * vec_a_y - t_y * vec_a_x = vec_b_x * c_b * vec_a_y - vec_b_y * c_b * vec_a_x
+    # t_x * vec_a_y - t_y * vec_a_x = (vec_b_x * vec_a_y - vec_b_y * vec_a_x) * c_b
+    # (t_x * vec_a_y - t_y * vec_a_x) / (vec_b_x * vec_a_y - vec_b_y * vec_a_x) = c_b
+
+    c_b = (target[0] * vec_a[1] - target[1] * vec_a[0]) / (vec_b[0] * vec_a[1] - vec_b[1] * vec_a[0])
+    c_a1 = (target[0] - vec_b[0] * c_b) / vec_a[0]
+    c_a2 = (target[1] - vec_b[1] * c_b) / vec_a[1]
+    if c_a1 != c_a2 or c_a1 % 1.0 != 0.0 or c_b % 1.0 != 0.0:
+        return (None, None)
+    return (int(c_a1), int(c_b))
+
+
 def part_2():
     total_tokens = 0
     claw_machines = [cm.split("\n") for cm in PUZZLE_INPUT.split("\n\n")]
     for cm in claw_machines:
-        b_a = [l[len("Button A: "):] for l in cm if l.startswith("Button A: ")][0].split(", ")
-        b_b = [l[len("Button B: "):] for l in cm if l.startswith("Button B: ")][0].split(", ")
-        prize = [l[len("Prize: "):] for l in cm if l.startswith("Prize: ")][0].split(", ")
-        b_a = {p[0]: int(p[1:]) for p in b_a}
-        b_b = {p[0]: int(p[1:]) for p in b_b}
-        prize = {p[0]: int(p[2:]) for p in prize}
-        b_a = (b_a["X"], b_a["Y"])
-        b_b = (b_b["X"], b_b["Y"])
-        prize = (prize["X"], prize["Y"])
+        b_a, b_b, prize = parse_claw_machine(cm)
         prize = (prize[0] + 10000000000000, prize[1] + 10000000000000)
 
-        if prize[0] * b_a[1] / b_a[0] == prize[1] and prize[0] / b_a[0] == int(prize[0] / b_a[0]):
-            total_tokens += 3 * int(prize[0] / b_a[0])
-            continue
-        if prize[0] * b_b[1] / b_b[0] == prize[1] and prize[0] / b_b[0] == int(prize[0] / b_b[0]):
-            total_tokens += int(prize[0] / b_b[0])
-            continue
-        diff_a_y = prize[1] - prize[0] * b_a[1] / b_a[0]
-        diff_b_y = prize[1] - prize[0] * b_b[1] / b_b[0]
-        if diff_a_y * diff_b_y > 0:
-            continue
-        c_a = 1
-        c_b = 1
-        errors = set()
-        while True:
-            vec = (b_a[0] * c_a + b_b[0] * c_b, b_a[1] * c_a + b_b[1] * c_b)
-            diff = prize[1] - prize[0] * vec[1] / vec[0]
-            if diff == 0:
-                f = prize[0] / vec[0]
-                total_tokens += c_a * 3 * f + c_b * f
-                break
-            if diff in errors:
-                break
-            errors.add(diff)
-            if diff > 0:
-                if b_a[1] / b_a[0] > b_b[1] / b_b[0]:
-                    c_a += 1
-                else:
-                    c_b += 1
+        c_a, c_b = get_vector_factors(prize, b_a, b_b)
+        if c_a is not None and c_b is not None:
+            total_tokens += c_a * 3 + c_b
     print(total_tokens)
 
 
