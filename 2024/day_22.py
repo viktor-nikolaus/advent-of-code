1d87bcf16b83d7f175dfcc189873002f9f2acb25
Viktor Nikolaus <Viktor.Nikolaus@gmx.net>
2024-12-22 09:27:28 +0100

    Add day 22 part 2

--- 2024/day_22.py
+++ 2024/day_22.py
@@ -22,23 +22,55 @@ def part_1(puzzle_input, iterations):
     return sum(generate_secret_number(int(l), iterations) for l in puzzle_input.split("\n"))
 
 
-def part_2(puzzle_input):
-    return ""
+def part_2(puzzle_input, max_iterations):
+    seq_dict = {}
+    for l in set(puzzle_input.split("\n")):
+        secret_number = int(l)
+        last_prices = [secret_number % 10]
+        seq_dict[l] = {}
+        for _ in range(max_iterations):
+            secret_number = generate_secret_number(secret_number, 1)
+            last_prices.append(secret_number % 10)
+            if len(last_prices) < 5:
+                continue
+            seq = (last_prices[-4] - last_prices[-5],
+                   last_prices[-3] - last_prices[-4],
+                   last_prices[-2] - last_prices[-3],
+                   last_prices[-1] - last_prices[-2])
+            if seq not in seq_dict[l]:
+                seq_dict[l][seq] = last_prices[-1]
+            last_prices.pop(0)
+
+    all_seqs = set()
+    for _, seqs in seq_dict.items():
+        all_seqs = all_seqs.union(seqs)
+    best_seq = None
+    best_bananas = None
+    for seq in all_seqs:
+        total = 0
+        for l, seqs in seq_dict.items():
+            if seq in seqs:
+                total += seqs[seq]
+        if best_bananas is None or best_bananas < total:
+            best_bananas = total
+            best_seq = seq
+    # print(best_seq, best_bananas)
+    return best_bananas
 
 
 def test():
     part_1_sample_result = 37327623
-    part_2_sample_result = ""
+    part_2_sample_result = 23
     result = True
     result &= test_sample_input(1, 1, part_1_sample_result, part_1, [2000])
-    result &= test_sample_input(1, 2, part_2_sample_result, part_2)
+    result &= test_sample_input(2, 2, part_2_sample_result, part_2, [2000])
     return result
 
 
 def main():
     puzzle_input = get_puzzle_input()
     print("Part 1:", part_1(puzzle_input, 2000))
-    print("Part 2:", part_2(puzzle_input))
+    print("Part 2:", part_2(puzzle_input, 2000))
 
 
 if __name__ == "__main__":
