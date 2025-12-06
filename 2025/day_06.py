8a5c1bea4e20c370fcbeb2592e9e1114214ee88a
Viktor Nikolaus <Viktor.Nikolaus@gmx.net>
2025-12-06 08:53:03 +0100

    2025 - Add day 6 part 2

--- 2025/day_06.py
+++ 2025/day_06.py
@@ -5,15 +5,43 @@ import re
 from common import get_puzzle_input, test_sample_input
 
 
-def parse_input(puzzle_input: str):
+def parse_input_1(puzzle_input: str):
     grid = [re.split(r"[ ]+", l.strip()) for l in puzzle_input.split("\n")]
     grid = [[int(i) for i in l] for l in grid[0:-1]] + [grid[-1]]
     return grid
 
 
-def part_1(puzzle_input):
-    puzzle_input = parse_input(puzzle_input)
+def transpose(grid):
+    result = [[grid[y][x] for y in range(len(grid))] for x in range(len(grid[0]))]
+    return result
+
+
+def parse_input_2(puzzle_input: str):
+    lines = puzzle_input.split("\n")
+    grid = []
+    k = 0
+    start_k = 0
+    for k in range(1, len(lines[0])):
+        if lines[-1][k] != " ":
+            end_k = k - 1
+            subgrid = [l[start_k:end_k] for l in lines]
+            subgrid = transpose(subgrid)
+            op = subgrid[0][-1]
+            subgrid = [int("".join(l[0:-1]).strip()) for l in subgrid] + [op]
+            grid.append(subgrid)
+            start_k = k
     
+    subgrid = [l[start_k:] for l in lines]
+    subgrid = transpose(subgrid)
+    op = subgrid[0][-1]
+    subgrid = [int("".join(l[0:-1]).strip()) for l in subgrid] + [op]
+    grid.append(subgrid)
+
+    return grid
+
+
+def part_1(puzzle_input):
+    puzzle_input = parse_input_1(puzzle_input)
     result = 0
     for x in range(len(puzzle_input[0])):
         operation = puzzle_input[-1][x]
@@ -31,16 +59,25 @@ def part_1(puzzle_input):
 
 
 def part_2(puzzle_input):
-    puzzle_input = parse_input(puzzle_input)
-
+    puzzle_input = parse_input_2(puzzle_input)
     result = 0
+    for l in puzzle_input:
+        operation = l[-1]
+        total = 0 if operation == "+" else 1
+        for i in l[0:-1]:
+            match operation:
+                case "+":
+                    total += i
+                case "*":
+                    total *= i
+        result += total
 
     return result
 
 
 def test():
     part_1_sample_result = 4277556
-    part_2_sample_result = 0
+    part_2_sample_result = 3263827
     result = True
     result &= test_sample_input(1, 1, part_1_sample_result, part_1)
     result &= test_sample_input(1, 2, part_2_sample_result, part_2)
